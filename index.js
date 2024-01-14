e552f9be089c2d9f5c55d482d8dac78a498350a7
iMMAN-TECH <115489899+i-MMANTECH@users.noreply.github.com>
2024-01-14 13:24:20 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// String Slicing -- usually represented by -> Slice()
-//                -- This extracts a section of a string and returns
-//                -- it as a new string, without modifying the original string
+// arrow function =>
+// we can also use the function in sorting out of arrays 
 
-let fullName = "iMMAN-TECH";
-let firstName;
-let lastName;
+let grades = [100, 50, 90, 60, 80, 70];
 
-
-//firstName = fullName.slice(0,5);
-// lastName = fullName.slice(6);
-
-firstName = fullName.slice(0, fullName.indexOf(" "));
-lastName = fullName.slice(fullName.indexOf(" ") + 1);
-
-console.log(firstName);
-console.log(lastName);
+grades.sort((x, y) => x - y);
+grades.forEach((element) => console.log(element));
